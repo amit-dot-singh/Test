a6d2075b4e11b0f84baaf4fba0af0fd5bd2fc616
amit-dot-singh <44203328+amit-dot-singh@users.noreply.github.com>
2024-03-09 18:11:37 +0000

    Variable changes

---
+++ JavaScriptTest/Variables.js
@@ -0,0 +1,7 @@
+const accountId = "124";
+let accountEmail = "test@test.com";
+var accountpassword = "asasds";
+accountCity = "London";
+let accountState;
+
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
